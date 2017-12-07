335fa822be5e4ef602833cc4fea6e9f785ad1daf
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2017-12-08 00:01:50 +0100

    Added few static methods for threads coordination operations and few comments in "README.md" file.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bmunit-extension
+
+Adding support of junit4 test rule to bmunit framework.
+
+TODO:
+- Adding static methods which invokes methods of class org.jboss.byteman.rule.helper.Helper which operate on static collections, like counters, lock etc.
+- Adding SpringBoot application demo which use BMUnitRule in integration tests
--- src/main/java/st/bmunit/extension/BMUnitUtils.java
+++ src/main/java/st/bmunit/extension/BMUnitUtils.java
@@ -18,4 +18,45 @@ public class BMUnitUtils {
     public static boolean joinWait(Object key, int count, long millis) {
         return new BMUnitHelperWithoutRuleReference().joinWait(key, count, millis);
     }
+
+    public static boolean createCounter(Object o){
+        return new BMUnitHelperWithoutRuleReference().createCounter(o);
+    }
+
+    public static boolean createCounter(Object o, int count){
+        return new BMUnitHelperWithoutRuleReference().createCounter(o, count);
+    }
+
+    public static boolean deleteCounter(Object o){
+        return new BMUnitHelperWithoutRuleReference().deleteCounter(o);
+    }
+
+    public static int incrementCounter(Object o, int amount){
+        return new BMUnitHelperWithoutRuleReference().incrementCounter(o, amount);
+    }
+
+    public static int incrementCounter(Object o){
+        return new BMUnitHelperWithoutRuleReference().incrementCounter(o);
+    }
+
+    public static int decrementCounter(Object o){
+        return new BMUnitHelperWithoutRuleReference().decrementCounter(o);
+    }
+
+    public static int readCounter(Object o){
+        return new BMUnitHelperWithoutRuleReference().readCounter(o);
+    }
+
+    public static int readCounter(Object o, boolean zero){
+        return new BMUnitHelperWithoutRuleReference().readCounter(o, zero);
+    }
+
+    public static boolean createRendezvous(Object identifier, int expected) {
+        return new BMUnitHelperWithoutRuleReference().createRendezvous(identifier, expected);
+    }
+
+    public static int rendezvous(Object identifier)
+    {
+        return new BMUnitHelperWithoutRuleReference().rendezvous(identifier);
+    }
 }
