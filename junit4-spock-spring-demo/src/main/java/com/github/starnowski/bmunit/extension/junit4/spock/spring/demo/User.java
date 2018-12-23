98dcbfb24174fa73128b5de0955b785933252f1a
Szymon Tarnowski <szymon.tarnowski.87@gmail.com>
2018-12-23 23:33:51 +0100

    #11 - Added the Spring boot dependencies

---
+++ junit4-spock-spring-demo/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/User.java
@@ -0,0 +1,8 @@
+package com.github.starnowski.bmunit.extension.junit4.spock.spring.demo;
+
+public class User {
+    private long id;
+    private String username;
+    private String password;
+    private String email;
+}
