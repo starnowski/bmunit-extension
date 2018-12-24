6c263770c63693d9a64a84e51a0752bc66c64519
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2018-12-24 15:48:34 +0100

    #11 - Added infrastructure components

---
+++ junit4-spock-spring-demo/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/services/MailService.java
@@ -0,0 +1,13 @@
+package com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.services;
+
+import com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    public void sendMessageToNewUser(UserDto dto)
+    {
+        //TODO
+    }
+}
---
+++ junit4-spock-spring-demo/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/services/UserService.java
@@ -0,0 +1,15 @@
+package com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.services;
+
+import com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public UserDto registerUser(UserDto dto)
+    {
+
+        //TODO
+        return null;
+    }
+}
