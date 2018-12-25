47981e89fbc2277872bdec2688096ae9dc41852f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2018-12-25 12:55:01 +0100

    #11 - Fixed dependency declaration required to use spock framework.

---
+++ junit4-spock-spring-demo/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/util/RandomHasGenerator.java
@@ -0,0 +1,12 @@
+package com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomHasGenerator {
+
+    public String compute() {
+        //TODO
+        return null;
+    }
+}
