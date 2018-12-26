fd5f5002a27b6d91179ba02d6ecb166c8425cc7f
Szymon Tarnowski <szymon.tarnowski.87@gmail.com>
2018-12-26 12:21:40 +0100

    #11 - Added implementation for RandomHashGenerator type

--- junit4-spock-spring-demo/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/util/RandomHashGenerator.java
+++ junit4-spock-spring-demo/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/util/RandomHashGenerator.java
@@ -2,11 +2,12 @@ package com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.util;
 
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class RandomHashGenerator {
 
     public String compute() {
-        //TODO
-        return null;
+        return UUID.randomUUID().toString();
     }
 }
