fca96e285cad58ab2472459c58b91040351eb50e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2018-12-26 12:12:04 +0100

    #11 - Added test for RandomHashGenerator type

---
+++ junit4-spock-spring-demo/src/test/groovy/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/util/BMUnitHelperWithStaticStringProperty.java
@@ -0,0 +1,16 @@
+package com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.util;
+
+import com.github.starnowski.bmunit.extension.utils.BMUnitHelperWithoutRuleReference;
+
+public class BMUnitHelperWithStaticStringProperty extends BMUnitHelperWithoutRuleReference {
+
+    private static String staticStringProperty;
+
+    public static String getStaticStringProperty() {
+        return staticStringProperty;
+    }
+
+    public static void setStaticStringProperty(String staticStringProperty) {
+        BMUnitHelperWithStaticStringProperty.staticStringProperty = staticStringProperty;
+    }
+}
