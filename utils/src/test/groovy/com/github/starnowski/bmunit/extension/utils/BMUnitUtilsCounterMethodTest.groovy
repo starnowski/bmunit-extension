d85b4adf803f77f9f1d408ebb357b77f112f4ae8
Szymon Tarnowski <szymon.tarnowski.87@gmail.com>
2019-01-01 20:20:44 +0100

    #3 - Added test for method st.bmunit.extension.BMUnitUtils#createCounter(Object, int)

--- utils/src/test/groovy/com/github/starnowski/bmunit/extension/utils/BMUnitUtilsCounterMethodTest.groovy
+++ utils/src/test/groovy/com/github/starnowski/bmunit/extension/utils/BMUnitUtilsCounterMethodTest.groovy
@@ -1,6 +1,7 @@
 package com.github.starnowski.bmunit.extension.utils
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static com.github.starnowski.bmunit.extension.utils.BMUnitUtils.createCounter
 import static com.github.starnowski.bmunit.extension.utils.BMUnitUtils.readCounter
@@ -18,5 +19,23 @@ class BMUnitUtilsCounterMethodTest extends Specification {
         then:
             result
             readCounter(counterName) == 0
+    }
+
+    @Unroll
+    def "should create counter #counterName with value #expectedValue" ()
+    {
+        when:
+            def result = createCounter(counterName, expectedValue)
+
+        then:
+            result
+            readCounter(counterName) == expectedValue
+
+        where:
+            counterName                         |   expectedValue
+            "BMUnitUtilsCounterMethodTest#2_1"  |   5
+            "BMUnitUtilsCounterMethodTest#2_2"  |   1
+            "BMUnitUtilsCounterMethodTest#2_3"  |   7
+            "BMUnitUtilsCounterMethodTest#2_4"  |   2
     }
 }
