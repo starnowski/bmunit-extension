837e470a53f48343c615cb506c707e3f90b74470
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-01-01 15:38:46 +0100

    #11 - Implemented the UserController type

--- junit4-spock-spring-demo/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/controllers/UserController.java
+++ junit4-spock-spring-demo/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.controllers;
 
 import com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.dto.UserDto;
+import com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @ResponseBody
     @PostMapping("/users")
     public UserDto post(@RequestBody UserDto dto)
     {
-        //TODO
-        return null;
+        return service.registerUser(dto);
     }
 }
