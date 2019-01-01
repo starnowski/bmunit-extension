929245421c266a75245530519176a49090c90fe5
Szymon Tarnowski <szymon.tarnowski.87@gmail.com>
2019-01-01 15:38:46 +0100

    #11 - Implemented the UserController type

--- junit4-spock-spring-demo/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/services/UserService.java
+++ junit4-spock-spring-demo/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/services/UserService.java
@@ -1,15 +1,33 @@
 package com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.services;
 
 import com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.dto.UserDto;
+import com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.model.User;
+import com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.repositories.UserRepository;
+import com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.util.RandomHashGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private RandomHashGenerator randomHashGenerator;
+    @Autowired
+    private MailService mailService;
+    @Autowired
+    private UserRepository repository;
+
+    @Transactional
     public UserDto registerUser(UserDto dto)
     {
-
-        //TODO
-        return null;
+        User user = new User().setEmail(dto.getEmail()).setPassword(passwordEncoder.encode(dto.getPassword())).setEmailVerificationHash(randomHashGenerator.compute());
+        user = repository.save(user);
+        UserDto response = new UserDto().setId(user.getId()).setEmail(user.getEmail());
+        mailService.sendMessageToNewUser(response, user.getEmailVerificationHash());
+        return response;
     }
 }
