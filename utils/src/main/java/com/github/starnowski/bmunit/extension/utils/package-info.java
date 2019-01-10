a9e4459ef6336fcbfa6909a35ab56bbe91001fe1
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-01-11 00:07:37 +0100

    #24 - Added package info for package "com.github.starnowski.bmunit.extension.utils".

---
+++ utils/src/main/java/com/github/starnowski/bmunit/extension/utils/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The Classes which allows to operate on mechanisms like counters, rendezvous, joins.
+ *
+ * @since 0.2.0
+ * @author starnowski
+ * @version 0.2.0
+ */
+package com.github.starnowski.bmunit.extension.utils;
