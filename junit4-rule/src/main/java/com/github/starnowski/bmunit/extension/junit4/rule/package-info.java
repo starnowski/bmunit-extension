eb4f38128a315cb6da54b6733b11f63a35572408
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-01-11 00:13:39 +0100

    #24 - Added package info for package "com.github.starnowski.bmunit.extension.junit4.rule".

---
+++ junit4-rule/src/main/java/com/github/starnowski/bmunit/extension/junit4/rule/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package contains implementation of type "org.junit.rules.MethodRule" for BMUnit library.
+ *
+ * @since 0.2.0
+ * @author starnowski
+ * @version 0.2.0
+ */
+package com.github.starnowski.bmunit.extension.junit4.rule;
