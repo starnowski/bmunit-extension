3089adc7fd66e680c1d198c86ef7dd7b17565dcc
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-12-17 21:08:11 +0100

    #55 - Added methods which controls object of type "CountDownLatch" for MailService.

--- native-java-testing-approach/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/concurrent/IApplicationCountDownLatch.java
+++ native-java-testing-approach/src/main/java/com/github/starnowski/bmunit/extension/junit4/spock/spring/demo/concurrent/IApplicationCountDownLatch.java
@@ -1,4 +1,10 @@
 package com.github.starnowski.bmunit.extension.junit4.spock.spring.demo.concurrent;
 
 public interface IApplicationCountDownLatch {
+
+    void mailServiceExecuteCountDownInHandleNewUserEventMethod();
+
+    void mailServiceWaitForCountDownLatchInHandleNewUserEventMethod();
+
+    void mailServiceRestCountDownLatchForHandleNewUserEventMethod();
 }
