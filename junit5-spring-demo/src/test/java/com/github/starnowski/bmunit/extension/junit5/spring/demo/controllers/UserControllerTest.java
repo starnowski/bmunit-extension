c86c9baa90954a0975546984ffed4ff4beb58784
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-02-16 13:42:01 +0100

    #61 - Fixed configuration of the SMTP server [skip travis]

--- junit5-spring-demo/src/test/java/com/github/starnowski/bmunit/extension/junit5/spring/demo/controllers/UserControllerTest.java
+++ junit5-spring-demo/src/test/java/com/github/starnowski/bmunit/extension/junit5/spring/demo/controllers/UserControllerTest.java
@@ -2,10 +2,13 @@ package com.github.starnowski.bmunit.extension.junit5.spring.demo.controllers;
 
 import com.github.starnowski.bmunit.extension.junit5.spring.demo.dto.UserDto;
 import com.github.starnowski.bmunit.extension.junit5.spring.demo.repositories.UserRepository;
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
 import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetup;
 import org.jboss.byteman.contrib.bmunit.BMRule;
 import org.jboss.byteman.contrib.bmunit.BMRules;
 import org.jboss.byteman.contrib.bmunit.BMUnitConfig;
+import org.jboss.byteman.contrib.bmunit.WithByteman;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,15 +26,15 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import static com.github.starnowski.bmunit.extension.junit5.spring.demo.util.DemoTestUtils.CLEAR_DATABASE_SCRIPT_PATH;
+import static com.github.starnowski.bmunit.extension.utils.BMUnitUtils.createJoin;
+import static com.github.starnowski.bmunit.extension.utils.BMUnitUtils.joinWait;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
 
-//@WithByteman
-//@ExtendWith(SpringExtension.class)
-//@RunWith(SpringRunner.class)
+@WithByteman
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(value = CLEAR_DATABASE_SCRIPT_PATH,
         config = @SqlConfig(transactionMode = ISOLATED),
@@ -42,10 +45,8 @@ import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.IS
 @EnableAsync
 public class UserControllerTest {
 
-//    @Rule
-//    public final GreenMailRule greenMail = new GreenMailRule(ServerSetupTest.SMTP_IMAP);
     @RegisterExtension
-    static GreenMailExtension greenMail = new GreenMailExtension();
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetup.verbose(ServerSetup.ALL)).withConfiguration(new GreenMailConfiguration().withUser("no.such.mail@gmail.com", "no.such.password"));
     @Autowired
     UserRepository userRepository;
     @Autowired
@@ -54,7 +55,6 @@ public class UserControllerTest {
     private int port;
 
     @Test
-//    @org.junit.Test
     @BMUnitConfig(verbose = true, bmunitVerbose = true)
     @BMRules(rules = {
             @BMRule(name = "signal thread waiting for mutex \"UserControllerTest.shouldCreateNewUserAndSendMailMessageInAsyncOperation\"",
@@ -69,12 +69,12 @@ public class UserControllerTest {
         assertThat(userRepository.findByEmail(expectedEmail)).isNull();
         UserDto dto = new UserDto().setEmail(expectedEmail).setPassword("XXX");
         RestTemplate restTemplate = new RestTemplate();
-//        createJoin("UserControllerTest.shouldCreateNewUserAndSendMailMessageInAsyncOperation", 1);
+        createJoin("UserControllerTest.shouldCreateNewUserAndSendMailMessageInAsyncOperation", 1);
         assertEquals(0, greenMail.getReceivedMessages().length);
 
         // when
         UserDto responseEntity = restTemplate.postForObject(new URI("http://localhost:" + port + "/users/"), (Object) dto, UserDto.class);
-//        joinWait("UserControllerTest.shouldCreateNewUserAndSendMailMessageInAsyncOperation", 1, 15000);
+        joinWait("UserControllerTest.shouldCreateNewUserAndSendMailMessageInAsyncOperation", 1, 15000);
 
         // then
         assertThat(userRepository.findByEmail(expectedEmail)).isNotNull();
@@ -82,5 +82,4 @@ public class UserControllerTest {
         assertThat(greenMail.getReceivedMessages()[0].getSubject()).contains("New user");
         assertThat(greenMail.getReceivedMessages()[0].getAllRecipients()[0].toString()).contains(expectedEmail);
     }
-
 }
