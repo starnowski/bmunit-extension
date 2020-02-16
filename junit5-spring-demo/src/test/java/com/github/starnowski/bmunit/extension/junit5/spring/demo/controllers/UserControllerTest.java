0535027b5fda307f6d6af8fc0ef4cbf455c339d4
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-02-16 12:46:46 +0100

    #61 - Added dependency to artifact "com.icegreen:greenmail-junit5" with version "1.6.0-SNAPSHOT" [skip travis]

--- junit5-spring-demo/src/test/java/com/github/starnowski/bmunit/extension/junit5/spring/demo/controllers/UserControllerTest.java
+++ junit5-spring-demo/src/test/java/com/github/starnowski/bmunit/extension/junit5/spring/demo/controllers/UserControllerTest.java
@@ -2,14 +2,17 @@ package com.github.starnowski.bmunit.extension.junit5.spring.demo.controllers;
 
 import com.github.starnowski.bmunit.extension.junit5.spring.demo.dto.UserDto;
 import com.github.starnowski.bmunit.extension.junit5.spring.demo.repositories.UserRepository;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
 import org.jboss.byteman.contrib.bmunit.BMRule;
 import org.jboss.byteman.contrib.bmunit.BMRules;
 import org.jboss.byteman.contrib.bmunit.BMUnitConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.web.client.RestTemplate;
@@ -21,6 +24,7 @@ import java.net.URISyntaxException;
 
 import static com.github.starnowski.bmunit.extension.junit5.spring.demo.util.DemoTestUtils.CLEAR_DATABASE_SCRIPT_PATH;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
@@ -35,12 +39,13 @@ import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.IS
 @Sql(value = CLEAR_DATABASE_SCRIPT_PATH,
         config = @SqlConfig(transactionMode = ISOLATED),
         executionPhase = AFTER_TEST_METHOD)
-//@EnableAsync
+@EnableAsync
 public class UserControllerTest {
 
 //    @Rule
 //    public final GreenMailRule greenMail = new GreenMailRule(ServerSetupTest.SMTP_IMAP);
-
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension();
     @Autowired
     UserRepository userRepository;
     @Autowired
@@ -65,7 +70,7 @@ public class UserControllerTest {
         UserDto dto = new UserDto().setEmail(expectedEmail).setPassword("XXX");
         RestTemplate restTemplate = new RestTemplate();
 //        createJoin("UserControllerTest.shouldCreateNewUserAndSendMailMessageInAsyncOperation", 1);
-//        assertEquals(0, greenMail.getReceivedMessages().length);
+        assertEquals(0, greenMail.getReceivedMessages().length);
 
         // when
         UserDto responseEntity = restTemplate.postForObject(new URI("http://localhost:" + port + "/users/"), (Object) dto, UserDto.class);
@@ -73,9 +78,9 @@ public class UserControllerTest {
 
         // then
         assertThat(userRepository.findByEmail(expectedEmail)).isNotNull();
-//        assertThat(greenMail.getReceivedMessages().length).isEqualTo(1);
-//        assertThat(greenMail.getReceivedMessages()[0].getSubject()).contains("New user");
-//        assertThat(greenMail.getReceivedMessages()[0].getAllRecipients()[0].toString()).contains(expectedEmail);
+        assertThat(greenMail.getReceivedMessages().length).isEqualTo(1);
+        assertThat(greenMail.getReceivedMessages()[0].getSubject()).contains("New user");
+        assertThat(greenMail.getReceivedMessages()[0].getAllRecipients()[0].toString()).contains(expectedEmail);
     }
 
 }
