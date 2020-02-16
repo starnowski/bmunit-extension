f20d756119ee1e6400a14e4a89d52e40365e8a80
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-02-16 12:02:20 +0100

    #61 - Code refactor [skip travis]

--- junit5-spring-demo/src/test/java/com/github/starnowski/bmunit/extension/junit5/spring/demo/DemoApplicationTest.java
+++ junit5-spring-demo/src/test/java/com/github/starnowski/bmunit/extension/junit5/spring/demo/DemoApplicationTest.java
@@ -1,12 +1,8 @@
 package com.github.starnowski.bmunit.extension.junit5.spring.demo;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-//@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT, classes = DemoApplication.class)
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTest {
 
