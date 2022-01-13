8c7a6207a137789c4d635eb676c06dba7b011840
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-13 01:36:24 +0100

    #101 - Added '-md md5' option to openssl invocation [travis skip]

--- .travis/release.sh
+++ .travis/release.sh
@@ -8,8 +8,8 @@ export GPG_DIR="$DIRNAME/deploy"
 
 openssl version
 # Decrypting key files
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -md md5 -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
 
 #mvn --settings .travis/settings.xml -DskipTests deploy -P sign-artifacts
 mvn clean install -DskipTests -P sign-artifacts -P main
