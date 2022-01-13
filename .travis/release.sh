26e7c4ff62d8ec90bd89d54f51bd56f6695664eb
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-13 01:14:57 +0100

    #101 - Printing openssl version [travis skip]

--- .travis/release.sh
+++ .travis/release.sh
@@ -6,6 +6,7 @@ DIRNAME="$(dirname $0)"
 # Setting gpg directory path
 export GPG_DIR="$DIRNAME/deploy"
 
+openssl version
 # Decrypting key files
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
